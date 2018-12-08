6ce2236c48fc549efc4b58e530ddf626682eee3b
IvanJeiden <ivanjesquivel@gmail.com>
2018-12-08 14:54:55 +0100

    Change in ProductType Database and Test
    Added object in ProductTypeTest.
    Added new methods in ProductTypeTest and ProductTypeDatabase.

--- lending_tree_application/src/test/java/org/lendingtree/project/ProductTypeTest.java
+++ lending_tree_application/src/test/java/org/lendingtree/project/ProductTypeTest.java
@@ -5,23 +5,46 @@ import org.junit.Test;
 public class ProductTypeTest {
 
     @Test
-    public void printProductTypeTest() {
-        int productTypeId = 1;
+    public void getProductTypeDescriptionTest() {
+        ProductType newProductType = new ProductType();
+        newProductType.setProductTypeId(1);
 
         try {
-            System.out.println(ProductTypeDatabase.getProductTypeDescription(productTypeId));
+            System.out.println(ProductTypeDatabase.getProductTypeDescription(newProductType.getProductTypeId()));
         } catch (Exception e){
             e.printStackTrace();
         }
     }
 
     @Test
-    public void changeProductTypeDescriptionTest() {
-        int productTypeId = 1;
-        String newProductTypeDescription = "Changed from the test";
+    public void getAllProductTypeDescriptionTest() {
+        try {
+            ProductTypeDatabase.getAllProductTypeDescription();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void updateProductTypeDescriptionTest() {
+        ProductType newProductType = new ProductType();
+        newProductType.setProductTypeId(1);
+        newProductType.setProductTypeDescription("Changed from the test");
 
         try {
-            ProductTypeDatabase.changeProductTypeDescription(productTypeId, newProductTypeDescription);
+            ProductTypeDatabase.updateProductTypeDescription(newProductType.getProductTypeId(), newProductType.getProductTypeDescription());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void createProductTypeTest() {
+        ProductType newProductType = new ProductType();
+        newProductType.setProductTypeDescription("New product type added from test");
+
+        try {
+            ProductTypeDatabase.createProductType(newProductType.getProductTypeDescription());
         } catch (Exception e){
             e.printStackTrace();
         }
