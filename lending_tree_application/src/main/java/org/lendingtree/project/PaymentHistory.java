172551beea780c31877f12c2d6f56059a9ef3494
chetan0541 <33390795+chetan0541@users.noreply.github.com>
2018-12-09 01:09:14 +0100

    Getters and Setters

--- lending_tree_application/src/main/java/org/lendingtree/project/PaymentHistory.java
+++ lending_tree_application/src/main/java/org/lendingtree/project/PaymentHistory.java
@@ -5,40 +5,118 @@ package org.lendingtree.project;
 import java.util.Date;
 import java.util.Scanner;
 
-public class PaymentHistory  {
+public class PaymentHistory {
 
-    protected  int loanId;
-    protected  int inputCustomerId;
-    protected  Float paymentAmount;
-    protected  Date paymentDate;
-    protected  String paymentType;
-    protected  int paymentHistoryId;
-
-
-
-
-
-    public void lenderPaymentHistoryinput() {
-        try {
-            Scanner enterInput = new Scanner(System.in);
-            System.out.println(" Enter the customer ID=");
-            inputCustomerId = Integer.parseInt(enterInput.nextLine());
-            PaymentDisplayDatabase.displayPaymentHistory(inputCustomerId);
+    private int inputCustomerId;
+    private Double paymentAmount;
+    private Date paymentDate;
+    private String paymentType;
+    private int paymentHistoryId;
+    private String customerLastName;
+    private String customerFirstName;
+    private String inputYN;
 
 
-        }catch (Exception e)
-        {
-            e.printStackTrace();
-            }
+    public int getInputCustomerId() {
+        return inputCustomerId;
+    }
 
+    public void setInputCustomerId(int inputCustomerId) {
+        this.inputCustomerId = inputCustomerId;
+    }
 
- }
+    public Date getPaymentDate() {
+        return paymentDate;
+    }
 
-    public static void main (String[] args){
-        PaymentHistory ph = new PaymentHistory();
-        ph.lenderPaymentHistoryinput();
+    public void setPaymentDate(Date paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public String getPaymentType() {
+        return paymentType;
+    }
+
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
+    }
+
+    public int getPaymentHistoryId() {
+        return paymentHistoryId;
+    }
+
+    public void setPaymentHistoryId(int paymentHistoryId) {
+        this.paymentHistoryId = paymentHistoryId;
+    }
+
+    public String getCustomerLastName() {
+        return customerLastName;
+    }
+
+    public void setCustomerLastName(String customerLastName) {
+        this.customerLastName = customerLastName;
+    }
+
+    public String getCustomerFirstName() {
+        return customerFirstName;
+    }
+
+    public void setCustomerFirstName(String customerFirstName) {
+        this.customerFirstName = customerFirstName;
+    }
+
+    public Double getPaymentAmount() {
+        return paymentAmount;
+    }
+
+    public void setPaymentAmount(Double paymentAmount) {
+        this.paymentAmount = paymentAmount;
     }
 
 
+    public void lenderPaymentHistory() {
+        try {
+            Scanner enterInput = new Scanner(System.in);
+            System.out.println("Would you like to see all the data of every customer? Y/N?");
+            inputYN = enterInput.nextLine();
 
+            if (inputYN != null) {
+                if (inputYN.equalsIgnoreCase("N")) {
+                    System.out.println("You chose No");
+                    System.out.println(" Enter the customer ID =  ");
+                    inputCustomerId = Integer.parseInt(enterInput.nextLine());
+                    PaymentHistoryDatabase.displayPaymentHistory(inputCustomerId);
+                } else if (inputYN.equalsIgnoreCase("Y")) {
+                    System.out.println(" You chose Yes");
+                    PaymentHistoryDatabase.displayPaymentHistoryAll();
+                } else {
+                    System.out.println("Incorrect input! Please choose either Y or N");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void customerPaymentHistory() {
+        int userId;
+
+        try {
+            userId = 1;
+            PaymentHistoryDatabase.displayPaymentHistoryCustomerDatabase(userId);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+
+  /*public static void main (String[] args){
+        PaymentHistory ph = new PaymentHistory();
+        ph.lenderPaymentHistory();
+    }*/
+
+
+    }
 }
